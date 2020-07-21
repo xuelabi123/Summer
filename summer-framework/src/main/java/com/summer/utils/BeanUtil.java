93d5515f8210be48fe28c93fa5bb4ae4d58e47b0
panyox <1070993165@qq.com>
2020-07-22 01:14:03 +0800

    add BeanUtil

---
+++ summer-framework/src/main/java/com/summer/utils/BeanUtil.java
@@ -0,0 +1,12 @@
+package com.summer.utils;
+
+/**
+ * @author panyox
+ * @date 2020/7/22 1:12 上午
+ */
+public class BeanUtil {
+
+    public static Object getInstance() {
+        return null;
+    }
+}
